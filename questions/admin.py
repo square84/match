2cd1d1c18beb9fdda62893f87cd0d399f11bfdf1
square <juher84@gmail.com>
2016-09-01 13:48:37 -0500

    9

--- questions/admin.py
+++ questions/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question,Answer
 
-admin.site.register(Question)
+class AnswerTabularInline(admin.TabularInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerTabularInline]
+    class Meta:
+        model= Question
+
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Answer)
