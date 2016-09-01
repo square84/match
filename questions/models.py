76645a4186f57ce019e245f60cc6223ab140d77a
square <juher84@gmail.com>
2016-09-01 13:41:10 -0500

    8

--- questions/models.py
+++ questions/models.py
@@ -9,3 +9,13 @@ class Question(models.Model):
 
     def __unicode__(self):
         return self.text[:10]
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+    draft = models.BooleanField(default= False)
+    timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
+
+    def __unicode__(self):
+        return self.text[:10]
