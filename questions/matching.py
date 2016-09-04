151a04f11947fc036588a89155d44ce2fb8d263c
square <juher84@gmail.com>
2016-09-04 15:37:31 -0500

    22

--- questions/matching.py
+++ questions/matching.py
@@ -8,9 +8,9 @@ user = User.objects.all()
 all_user_answer = UserAnswer.objects.all().order_by("user__id")
 
 
-def get_match(usera, userb):
-    user_a_answers = UserAnswer.objects.filter(user=usera)[0]
-    user_b_answers = UserAnswer.objects.filter(user=userb)[0]
+def get_match(user_a, user_b):
+    user_a_answers = UserAnswer.objects.filter(user=user_a)[0]
+    user_b_answers = UserAnswer.objects.filter(user=user_b)[0]
 
     if user_a_answers.question.id == user_b_answers.question.id:
         user_a_answer = user_a_answers.my_answer
@@ -18,10 +18,15 @@ def get_match(usera, userb):
         user_b_answer = user_b_answers.my_answer
         user_b_pref = user_b_answers.their_answer
 
+        user_a_total_awared = 0
+        user_b_total_awared = 0
+
         if user_a_answer == user_b_pref:
+            user_b_total_awared += user_b_answers.their_points
             print "%s fits with %s's preference" % (user_a_answers.user.username, user_b_answers.user.username)
 
         if user_a_pref == user_b_answer:
+            user_a_total_awared += user_a_answers.their_points
             print " %s fits with %s's preference" % (user_a_answers.user.username, user_b_answers.user.username)
 
         if user_a_answer == user_b_pref and user_a_pref == user_b_answer:
