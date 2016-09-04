3387db7025f286542ba8e16b894b7e8032ab44f1
square <juher84@gmail.com>
2016-09-04 15:49:30 -0500

    23

--- questions/matching.py
+++ questions/matching.py
@@ -31,3 +31,23 @@ def get_match(user_a, user_b):
 
         if user_a_answer == user_b_pref and user_a_pref == user_b_answer:
             print "this is an ideal answer for both"
+
+
+def get_points(user_a,user_b):
+    a_answers = UserAnswer.objects.filter(user=user_a)[0]
+    b_answers = UserAnswer.objects.filter(user=user_b)[0]
+    a_total_awarded = 0
+    a_points_possible = 0
+    if a_answers.question.id == b_answers.question.id:
+        a_pref = a_answers.their_answer
+        b_answers = b_answers.my_answer
+        if a_pref == b_answers:
+            '''
+            awards points for correct answer
+            '''
+            a_total_awarded += a_answers.their_points
+        '''
+        assiging total points
+        '''
+        a_points_possible += a_answers.their_points
+    print "%s has awarded %s points of %s to %s" % (user_a,a_total_awarded,a_points_possible,user_b)
