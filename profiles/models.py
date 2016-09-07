c947311096b9e2ac6390ada1643e02c52e307a7a
square <juher84@gmail.com>
2016-09-07 13:19:57 -0500

    39

--- profiles/models.py
+++ profiles/models.py
@@ -1,19 +1,26 @@
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
 
-def upload_location(instance,filename):
-    #extension = filename.split(".")[1]
+
+def upload_location(instance, filename):
+    # extension = filename.split(".")[1]
     location = str(instance.user.username)
-    return "%s/%s" % (location,filename)
+    return "%s/%s" % (location, filename)
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User) #solo tendra un profile
-    location = models.CharField(max_length=120,null=True,blank=True)
-    picture = models.ImageField(upload_to=upload_location,null=True,blank=True)
+    user = models.OneToOneField(User)  # solo tendra un profile
+    location = models.CharField(max_length=120, null=True, blank=True)
+    picture = models.ImageField(upload_to=upload_location, null=True, blank=True)
 
     def __unicode__(self):
         return self.user.username
+
+    def get_absolute_url(self):
+        url = reverse("profile",kwargs={"username": self.user.username})
+        return url
